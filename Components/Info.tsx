c5501baf5b9647a7ad6b9fb289d08c46cefc9dd9
ThyagarajuPathur <thyagaraju.ptr@gmail.com>
2024-10-25 02:02:25 +0530

    Initial Commit

---
+++ Components/Info.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default function Info() {
+  return (
+    <div className="Info">
+      <img src="" alt="" />
+      <h1>Thyagaraju Pathuru</h1>
+      <h3>Full Stack Dev</h3>
+      <h5>Thyagaraju.website</h5>
+      <div className="btns">
+        <button className="btn-email">Email</button>
+        <button className="btn-linkedIn">LinkedIn</button>
+      </div>
+    </div>
+  );
+}
---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import Info from "../Components/Info";
+
+function App() {
+  return (
+    <>
+      <div className="App">
+        <Info />
+      </div>
+    </>
+  );
+}
+
+export default App;
